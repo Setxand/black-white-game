3eb4195449a572b61df6dbc035a173c8e2465125
setxand <setxand@gmail.com>
2019-08-08 10:05:56 +0300

    Initial commit

---
+++ src/main/java/com/blackonwhite/BlackOnWhiteGameApplication.java
@@ -0,0 +1,13 @@
+package com.blackonwhite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackOnWhiteGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackOnWhiteGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackonwhite/BlackOnWhiteGameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blackonwhite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlackOnWhiteGameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
